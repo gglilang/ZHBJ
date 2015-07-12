d1126ea97bf6df636eef2949eff214bf15e34b94
朗 <951522473@qq.com>
2015-07-12 15:39:48 +0800

    导入xutils包成功

--- app/src/main/java/com/lang/zhbj/fragment/ContentFragment.java
+++ app/src/main/java/com/lang/zhbj/fragment/ContentFragment.java
@@ -5,16 +5,25 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.lang.zhbj.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * Created by Lang on 2015/7/11.
  */
 public class ContentFragment extends BaseFragment {
 
-    private RadioGroup rb_group;
+    @ViewInject(R.id.rg_group)
+    private RadioGroup rg_group;
     @Override
     public View initView() {
         View view = View.inflate(mActivity, R.layout.fragment_content, null);
+        ViewUtils.inject(this, view);   // 注入view和事件
         return view;
+    }
+
+    @Override
+    public void initData() {
+        rg_group.check(R.id.rb_home);   // 默认勾选页
     }
 }
